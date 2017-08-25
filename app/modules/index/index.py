691f051f53a04ffeb8db49b5cd8e81a827acdfa4
East_wu <wudong@eastwu.cn>
2017-08-26 00:56:48 +0800

    index change

--- app/modules/index/index.py
+++ app/modules/index/index.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 
 from app.models.blog.recommend import HomeRecommend
 from app.models.account.info import UserInfo
-
+from app.models.blog.article_meta import BlogArticleMeta
+from app.models.blog.article import BlogArticle
 
 PAGE_COUNT = 10
 
@@ -18,9 +19,25 @@ def index_handler(request):
     data["user_info"] = account
 
     data["recommend_list"] = HomeRecommend.query_recommend_list(0)
+    # 阅读榜单
+    data["hit_list"] = get_format_top_article_list(order="hit")
+    # 点赞榜单
+    data["like_list"] = get_format_top_article_list(order="like")
+    # 评论榜单
+    data["comment_list"] = get_format_top_article_list(order="comment")
 
     return render(request, "index/index.html", data)
 
 
-
-
+def get_format_top_article_list(order="hit"):
+    """
+    查询格式化的榜单信息
+    :param order: 排序规则
+    """
+    query_list = BlogArticleMeta.query_top_list(order)
+    hit_top_list = list()
+    for meta in query_list:
+        article = BlogArticle.query_article_by_id(meta.share_id)
+        if article:
+            hit_top_list.append(article)
+    return hit_top_list
